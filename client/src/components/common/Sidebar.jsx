3b65e20873b21b259cc0a625cb712b9454e624ff
rajeshrio9885 <nirmalvicky62@gmail.com>
2024-12-09 01:20:06 +0530

    width

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -44,8 +44,8 @@ const Sidebar = () => {
 	})
 
 	return (
-		<div className='md:flex-[2_2_0] w-18 max-w-52'>
-			<div className='sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full'>
+		<div  className='md:flex-[2_2_0] w-18 max-w-30'>
+			<div style={{width:"15px"}} className='sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-10 md:w-full'>
 				<Link to='/' className='flex justify-center md:justify-start'>
 					<XSvg className='px-2 w-12 h-12 rounded-full fill-white hover:bg-stone-900' />
 				</Link>
